a0b74e01555e2cbba3017c8cce2a85a7be055634
Johannes Deml <public@deml.io>
2020-01-31 19:05:17 +0100

    Add rigidbody sleep

---
+++ Assets/_Project/02_Core/02_Scripts/RigidbodySleep.cs
@@ -0,0 +1,52 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InteractableLevelBox.cs" company="Supyrb">
+//   Copyright (c) 2020 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+
+namespace Supyrb
+{
+	[RequireComponent(typeof(Rigidbody))]
+	public class RigidbodySleep : MonoBehaviour
+	{
+		[SerializeField]
+		private Rigidbody rb = null;
+
+		[SerializeField]
+		private float sleepThreshold = 0.4f;
+
+		[SerializeField]
+		private bool sleepOnEnable = true;
+		
+		void Awake()
+		{
+			rb.sleepThreshold = sleepThreshold;
+		}
+
+		private void OnEnable()
+		{
+			if (sleepOnEnable)
+			{
+				rb.velocity = Vector3.zero;
+				rb.angularVelocity = Vector3.zero;
+				rb.Sleep();
+			}
+		}
+
+		#if UNITY_EDITOR
+		private void Reset()
+		{
+			if(rb == null) {
+				rb = GetComponent<Rigidbody>();
+			}
+		}
+		#endif
+	}
+}
