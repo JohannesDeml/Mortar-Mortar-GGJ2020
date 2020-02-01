40a91a000041163d199dea3d4f8dd2d70bfacea1
Johannes Deml <public@deml.io>
2020-02-02 00:39:28 +0100

    Add game events aka open pandora's box 😱

---
+++ Assets/_Project/02_Scripts/Prototyping/GameEvent.cs
@@ -0,0 +1,71 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Supyrb
+{
+	[CreateAssetMenu]
+	public class GameEvent : ScriptableObject
+	{
+		public delegate void GameEventDelegate(GameEvent raisedEvent);
+
+		public static event GameEventDelegate EventRaised;
+		
+		/// <summary>
+		/// The list of listeners that this event will notify if it is raised.
+		/// </summary>
+		private readonly List<GameEventListener> eventListeners =
+			new List<GameEventListener>();
+
+		[Button()]
+		public void Raise()
+		{
+			if (EventRaised != null)
+			{
+				EventRaised(this);
+			}
+			
+			for (int i = eventListeners.Count - 1; i >= 0; i--)
+			{
+				eventListeners[i].OnEventRaised();
+			}
+		}
+
+		public void RegisterListener(GameEventListener listener)
+		{
+			if (!eventListeners.Contains(listener))
+			{
+				eventListeners.Add(listener);
+			}
+		}
+
+		public void UnregisterListener(GameEventListener listener)
+		{
+			if (eventListeners.Contains(listener))
+			{
+				eventListeners.Remove(listener);
+			}
+		}
+
+		#if UNITY_EDITOR
+		[Button()]
+		private void PrintAllListeners()
+		{
+			Debug.Log(eventListeners.Count);
+			for (int i = 0; i < eventListeners.Count; i++)
+			{
+				var listener = eventListeners[i];
+				Debug.Log(i + ": " + listener.gameObject.name, listener);
+			}
+		}
+		#endif
+	}
+}
---
+++ Assets/_Project/02_Scripts/Prototyping/StateMachineEvents.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StateMachineEvents.cs" company="Supyrb">
+//   Copyright (c) 2017 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   send@johannesdeml.com
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Supyrb
+{
+	public class StateMachineEvents : StateMachineBehaviour
+	{
+		public delegate void StateMachineDelegate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
+
+		public event StateMachineDelegate OnAnimationStateEnter;
+		public event StateMachineDelegate OnAnimationStateExit;
+		public GameEvent EnterEvent;
+		public GameEvent ExitEvent;
+
+		public UnityEvent OnEnter = null;
+		public UnityEvent OnExit = null;
+
+		// OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+		public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if (EnterEvent != null)
+			{
+				EnterEvent.Raise();
+			}
+
+			OnEnter.Invoke();
+			
+			if (OnAnimationStateEnter != null)
+			{
+				OnAnimationStateEnter(animator, stateInfo, layerIndex);
+			}
+		}
+
+		// OnStateExit is called before OnStateExit is called on any state inside this state machine
+		public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if (ExitEvent != null)
+			{
+				ExitEvent.Raise();
+			}
+			
+			OnExit.Invoke();
+
+			if (OnAnimationStateExit != null)
+			{
+				OnAnimationStateExit(animator, stateInfo, layerIndex);
+			}
+		}
+
+
+		// OnStateMachineEnter is called when entering a statemachine via its Entry Node
+		//override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash){
+		//
+		//}
+
+		// OnStateMachineExit is called when exiting a statemachine via its Exit Node
+		//override public void OnStateMachineExit(Animator animator, int stateMachinePathHash) {
+		//
+		//}
+	}
+}
