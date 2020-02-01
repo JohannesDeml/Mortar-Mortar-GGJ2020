f39b3f33b28f0e79597a3029ba65d9f470b21a30
Johannes Deml <public@deml.io>
2020-02-01 20:35:20 +0100

    Add new scene 🤫

---
+++ Assets/_Project/02_Scripts/Signals/SignalEvents.cs
@@ -0,0 +1,76 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SignalEvents.cs" company="Supyrb">
+//   Copyright (c)  Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Supyrb
+{
+	public class SignalEvents : MonoBehaviour
+	{
+		[SerializeField]
+		private UnityEvent startGame = null;
+		
+		[SerializeField]
+		private UnityEvent gameOverFail = null;
+		
+		[SerializeField]
+		private UnityEvent gameOverWin = null;
+		
+		[SerializeField]
+		private UnityEvent restartLevel = null;
+		
+		private StartGameSignal startGameSignal;
+		private GameOverSignal gameOverSignal;
+		private RestartLevelSignal restartLevelSignal;
+		
+		
+		private void Awake()
+		{
+			Signals.Get(out startGameSignal);
+			Signals.Get(out gameOverSignal);
+			Signals.Get(out restartLevelSignal);
+
+			startGameSignal.AddListener(OnStartGame);
+			gameOverSignal.AddListener(OnGameOverSignal);
+			restartLevelSignal.AddListener(OnRestartLevelSignal);
+		}
+
+		private void OnDestroy()
+		{
+			startGameSignal.RemoveListener(OnStartGame);
+			gameOverSignal.RemoveListener(OnGameOverSignal);
+			restartLevelSignal.RemoveListener(OnRestartLevelSignal);
+		}
+
+		private void OnStartGame()
+		{
+			startGame.Invoke();
+		}
+		
+		private void OnGameOverSignal(bool success)
+		{
+			if (success)
+			{
+				gameOverWin.Invoke();
+			}
+			else
+			{
+				gameOverFail.Invoke();
+			}
+		}
+
+		private void OnRestartLevelSignal()
+		{
+			restartLevel.Invoke();
+		}
+	}
+}
