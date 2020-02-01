3f5d74491bc7b66b8015c77020c44349a28bce02
Johannes Deml <public@deml.io>
2020-02-01 18:54:06 +0100

    Add projectile collision calculation

--- Assets/_Project/02_Scripts/LineBehaviour.cs
+++ Assets/_Project/02_Scripts/LineBehaviour.cs
@@ -1,29 +1,86 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class LineBehaviour : MonoBehaviour
 {
-    LineRenderer lineRenderer;
-    public GameObject player;
+    [SerializeField]
+    private LineRenderer lineRenderer = null;
 
     [SerializeField]
     private float totalSimulatedTime = 1.0f;
-    // Start is called before the first frame update
+
+    [SerializeField, Min(1)]
+    private int maxPoints = 15;
+
+    [SerializeField]
+    private LayerMask layerMask = default;
+
+    [SerializeField]
+    private Transform impactTransform = null;
+    
+    private Ray ray;
+
+    private RaycastHit[] hits;
+    
     void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        ray = new Ray();
+        hits = new RaycastHit[1];
+        impactTransform.gameObject.SetActive(false);
+    }
+    
+    public void UpdateWithForce(Vector3 force)
+    {
+        lineRenderer.positionCount = maxPoints;
+
+        var lastLocalPosition = Vector3.zero;
+        lineRenderer.SetPosition(0,lastLocalPosition);
+        for (int i = 1; i < maxPoints; i++)
+        {
+            var time = ((float)i / maxPoints) * totalSimulatedTime;
+            Vector3 localPosition = ProjectileMotion(force, time);
+            
+            var lastNextVector = transform.rotation * (localPosition - lastLocalPosition);
+            ray.origin = transform.rotation * lastLocalPosition + transform.position;
+            ray.direction = lastNextVector.normalized;
+            var length = lastNextVector.magnitude;
+            
+            // Debug.DrawRay(ray.origin, (ray.direction * length), Color.green, 0.1f);
+            var hitCount = Physics.RaycastNonAlloc(ray, hits, length, layerMask, QueryTriggerInteraction.Ignore);
+            if (hitCount > 0)
+            {
+                var hit = hits[0];
+                var localHitPosition = Quaternion.Inverse(transform.rotation) * (hit.point - transform.position);
+                lineRenderer.SetPosition(i,localHitPosition);
+                lineRenderer.positionCount = i + 1;
+                PositionCollisionGizmo(hit);
+                return;
+            }
+            lineRenderer.SetPosition(i,localPosition);
+            lastLocalPosition = localPosition;
+        }
+        
+        impactTransform.gameObject.SetActive(false);
+    }
+
+    private void PositionCollisionGizmo(RaycastHit hit)
+    {
+        impactTransform.gameObject.SetActive(true);
+        impactTransform.position = hit.point;
+    }
+
+    public Vector3 ProjectileMotion(Vector3 force, float timeToDistance){        
+        Vector3 position = force * timeToDistance + Physics.gravity * (0.5f * timeToDistance * timeToDistance);
+        return position;
+    }
+    
+    #if UNITY_EDITOR
+
+    private void Reset()
     {
         lineRenderer = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
-    public void UpdateWithForce(Vector3 force){
-        for (int i = 0; i < lineRenderer.positionCount; i++)
-        {
-            var time = ((float)i / lineRenderer.positionCount) * totalSimulatedTime;
-            Vector3 flyingPosition = ProjectileMotion(force, time);
-            lineRenderer.SetPosition(i,flyingPosition);
-        }
-    }
-    public Vector3 ProjectileMotion(Vector3 force, float timeToDistance){        
-        Vector3 currentVelocity = force * timeToDistance + Physics.gravity * (0.5f * timeToDistance * timeToDistance);
-        return currentVelocity;
-    }
+    #endif
 }
