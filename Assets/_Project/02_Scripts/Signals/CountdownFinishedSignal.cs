96500ec7b7c857f12ffd93db1145118a21959743
Johannes Deml <public@deml.io>
2020-02-02 11:04:17 +0100

    Add countdown screen

---
+++ Assets/_Project/02_Scripts/Signals/CountdownFinishedSignal.cs
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CountdownFinishedSignal.cs" company="Supyrb">
+//   Copyright (c) 2020 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Supyrb
+{
+	public class CountdownFinishedSignal : Signal
+	{
+	}
+}
